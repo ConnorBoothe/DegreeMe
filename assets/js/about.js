d9f195a6be214819202bfdaa507e6c5183f4d570
ConnorBoothe1 <47653354+ConnorBoothe1@users.noreply.github.com>
2020-09-09 13:41:00 -0400

    About is mobile responsive

--- assets/js/about.js
+++ assets/js/about.js
@@ -6,11 +6,4 @@ $(document).ready(function(){
     $('.scrollArrow').on('click', function(){
     scrollToAnchor('aboutScroll');
     })
-
-    $('.scrollArrow').on('mouseover', function(){
-        $(this).animate({marginTop:'5px'})
-        })
-    $('.scrollArrow').on('mouseleave', function(){
-        $(this).delay(100).animate({marginTop:'0px'})
-        })
 });
