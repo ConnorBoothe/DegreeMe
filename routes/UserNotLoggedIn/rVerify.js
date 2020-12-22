d1e1ff73e2d9d2e5c9da286944849f85654c273a
Connor Boothe <connorboothe@Connors-MacBook-Pro.local>
2020-12-22 01:56:45 -0500

    Replaced email code with email function

--- routes/UserNotLoggedIn/rVerify.js
+++ routes/UserNotLoggedIn/rVerify.js
@@ -3,15 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const session = require('express-session');
-const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
-const unirest = require("unirest");
-const mail = unirest("POST", "https://api.sendgrid.com/v3/mail/send");
 var stripe = require('stripe')(process.env.STRIPE_KEY);
 //DBs used
 const userDB = require('../../models/Database/UserDB');
+const EmailFunction = require('../../models/classes/EmailFunction');
 //instantiate DBs
 const UserDB = new userDB();
+const emailFunction = new EmailFunction();
 //use session and body parser
 router.use(session({
     secret: 'iloveu',
@@ -95,50 +94,25 @@ router.post("/updateStatus", function(req, res){
 router.post("/sendEmail", function(req, res){
     //generate random code and save it to the DB
     var activationCode = Math.floor(Math.random() * 10000);
-    UserDB.updateActivationCode(req.body.email1,  activationCode);
-    UserDB.getUserByEmail(req.body.email1).exec((err, docs)=>{
-        mail.headers({
-            "content-type": "application/json",
-            "authorization": process.env.SENDGRID_API_KEY,
-            });
-        
-            mail.type("json");
-            mail.send({
-            "personalizations": [
-                {
-                    "to": [
-                        {
-                            "email": docs[0].email,
-                        }
-                ],
-                    "dynamic_template_data": {
-                        "subject": "Account Confirmation",
-                        "name": docs[0].first_name,
-                        "code": activationCode,
-                        "email": docs[0].email,
-                
-                },
+    UserDB.updateActivationCode(req.body.email1,  activationCode)
+    .then(()=>{
+        UserDB.getUserByEmail(req.body.email1).then((user)=>{
+            if(user.length > 0) {
+                emailFunction.createEmail(user[0].email, "verifyEmail",
+                 [activationCode, user[0]])
+                 .then(()=>{
+                    res.redirect("/login?message=New Email Sent");
+                 })
+                 .catch((err)=>{
+                    res.redirect("/login?message=There was a problem sending an email to your account. Try again!");
+                     console.log(err)
+                 })
             }
-            ],
-                "from": {
-                    "email": "notifications@degreeme.io",
-                    "name": "DegreeMe"
-            },
-                "reply_to": {
-                    "email": "noreply@degreeme.io",
-                    "name": "No Reply"
-            },
-                "template_id": "d-e54827ff53514c15969d2e52db32e13d"
-            });
         
-            mail.end(function (res) {
-                // if (res.error) throw new Error(res.error);
-        
-            console.log(res.body);
-            });
+         })
     })
+   
 
    
-    res.redirect("/login?message=New Email Sent");
 });
 module.exports = router;
