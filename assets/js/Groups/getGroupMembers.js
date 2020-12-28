4b4fb8539a4d4bc90c57dc848a14f34cf068c894
Connor Boothe <connorboothe@Connors-MacBook-Pro.local>
2020-12-28 14:20:06 -0500

    Added function to append members to the view

--- assets/js/Groups/getGroupMembers.js
+++ assets/js/Groups/getGroupMembers.js
@@ -1,4 +1,21 @@
-$(document).ready(()=>{
+function appendGroupMembers(res){
+    console.log("RES:" +res.Members)
+    var members = "";
+    for(x in res.Members){
+            members += 
+            '<div class="messageHandle">'+
+            '<span class="online"></span></a>'+
+            '<a class="member-tooltip" href="/user/'+res.Members[x].MemberHandle+'" data-toggle="tooltip" data-placement="bottom"'+
+           'title="Online">'+
+            '<img class="messageSideBarImg" src="'+res.Members[x].MemberImage+'" />'+
+            '</a>'+
+                '<span class="userHandleTxt">'+res.Members[x].MemberHandle+'</span>'+
+           
+        '</div>';
+        
+    }
+    return members;
+}
     $(document).ready(()=>{
         var groupId = window.location.href.split("/")[4];
         $.ajax({
@@ -8,7 +25,7 @@ $(document).ready(()=>{
                
             }
             }).done(function(res) {
-                console.log(res)
+                console.log(res.Members)
+                $(".membersRightSideBar").append(appendGroupMembers(res));
             });
     })
-})
