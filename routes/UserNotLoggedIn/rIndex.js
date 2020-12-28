ec7a5193885af329b0f25ad626ce1e7d000aff77
Connor Boothe <connorboothe@Connors-MacBook-Pro.local>
2020-12-28 00:04:46 -0500

    Cleaned up index route

--- routes/UserNotLoggedIn/rIndex.js
+++ routes/UserNotLoggedIn/rIndex.js
@@ -1,22 +1,7 @@
 //packages
 const express = require('express');
 const router = express.Router();
-const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true,useUnifiedTopology: true },function(err){
 
-});
-const session = require('express-session'); //used to manipulate the session
-var MongoStore = require('connect-mongo')(session);
-router.use(session({
-    store: new MongoStore({
-       mongooseConnection: mongoose.connection
-      }),
-      secret: 'toolbox1217!',
-      resave: true,
-      saveUninitialized: true,
-      cookie: { secure: false,
-          maxAge:  6*60*60*1000 },
-    }));
 //render the index page
 router.get('/', function(req, res){
     if(req.session.userId){
