76f80b2939f70d6b39dd35b0606944dd9f1e74b1
Connor Boothe <connorboothe@Connors-MacBook-Pro.local>
2020-12-31 02:47:45 -0500

    Tweaked front end js for messages to work for groups as well

--- assets/js/Messages/messageFunctions.js
+++ assets/js/Messages/messageFunctions.js
@@ -23,7 +23,7 @@ function readURL(input, imageArray, inputVal) {
     }
 }
 //save image to firebase and save to DB using websocket
-function sendImage(id, sender, senderImg, imageArray, imageNumber) {
+function sendImage(id, sender, senderImg, imageArray, imageNumber, chat) {
     $(".image-attachments").html("");
     chat.append('<div class="sent-wrapper"><div class="containMessageSent">' +
         '<div class="msg_">' +
@@ -67,7 +67,7 @@ function sendMessage(id, sender, senderImg) {
         hasImage: false
     });
 }
-function appendYoutubeDetails(thumbnail, link, title) {
+function appendYoutubeDetails(thumbnail, link, title, chat) {
     chat.append('<div class="sent-wrapper"><div class="containMessageSent">' +
                 '<div class="msg_ ">' +
                 '<a target="_blank" href="'+link+'">'+
@@ -87,8 +87,7 @@ function appendYoutubeDetails(thumbnail, link, title) {
 
 }
 //append chat to the DOM on new message
-function appendChat(threadId, data, messageId) {
-    console.log(messageId)
+function appendChat(threadId, data, messageId, chatContainer) {
     if (data.msg.threadId === threadId) {
         if (data.msg.sender === $(".userProfileName").eq(0).text()) {
             //if youtube link
@@ -124,20 +123,19 @@ function appendChat(threadId, data, messageId) {
                   });
             }
             else {
-                chat.append('<div class="sent-wrapper"><div class="containMessageSent">' +
+                chatContainer.append('<div class="sent-wrapper"><div class="containMessageSent">' +
                 '<div class="msg_ ">' +
                 '<p class="messageBody sentMsg bg-primary">' + data.msg.content + '</p></div>' +
                 '<p class="msg-date msg-date-sent"></p>' +
                 '</div></div>');
                 $(".messageBlock").scrollTop($(".messageBlock")[0].scrollHeight);
-
             }
             
         } else {
             if(youtubeRegEx.test(data.msg.content)){
                 alert("YOUTUBE!!!")
             }
-            chat.append('<div class="containMessageReceived">' +
+            chatContainer.append('<div class="containMessageReceived">' +
                 '<img class="messageImg" src="' + data.msg.senderImg +
                 '" /><p class="sender_handle"' +
                 data.msg.sender +
@@ -232,4 +230,101 @@ function validateImage(image){
         return false;
     }
     
+}
+function prependNext50(messages, handle, block) {
+    var messageHTML = '<div class="messageTop'+block+'"></div>';
+    for (var x = messages.length - 1; x >= 0; x--) {
+        // check if session handle === sender
+        if (handle === messages[x].sender) {
+            if (messages[x].type == "file") {
+                messageHTML += '<div class="sent-wrapper">' +
+                    '<div class="containMessageSent">' +
+                    '<div class="msg_">' +
+                    '<p class="msgSender"></p>' +
+                    '<p class="messageBody sentMsg bg-primary message-attachment">';
+                if (messages[x].content.includes(".pdf")) {
+                    messageHTML += '<iframe class="pdf-iframe" src="' + messages[x].content + '"></iframe>' +
+                        '<p class="text-center"><a target="_blank" class="text-light"' +
+                        'href="' + messages[x].content + '">View Full Screen</a></p>';
+                } else {
+                    messageHTML += '<a target="_blank" href="<%=messages[x].content%>">' +
+                        '<img src="' + messages[x].content + '" />' +
+                        '</a>';
+                }
+                messageHTML += '</p>' +
+                    '</div>' +
+                    '<p class="msg-date msg-date-sent">' + formatTime(new Date(messages[x].dateTime)) +
+                    '</p>' +
+                    '</div>' +
+                    '</div>';
+            } else {
+                messageHTML += '<div class="sent-wrapper">' +
+                    '<div class="containMessageSent">' +
+                    '<div class="msg_">';
+                if (messages[x].youtubeData[0] != undefined) {
+                    messageHTML += '<a target="_blank" href="' + messages[x].youtubeData[0].link + '">' +
+                        '<div class="messageBody sentMsg bg-primary">' +
+                        '<img class="yt-logo" src= "../assets/img/yt-logo.svg"/>' +
+                        '<div class="thumbnail-container">' +
+                        '<img src="' + messages[x].youtubeData[0].thumbnail + '" class="yt-thumbnail"/>' +
+                        '</div>' +
+                        '<span class="text-light yt-title">' + unescapeApostrophe(messages[x].youtubeData[0].title) + '</span>' +
+                        '</div>' +
+                        '</a>';
+                } else {
+                    messageHTML += '<p class="messageBody sentMsg bg-primary">' + messages[x].content + '</p>';
+                }
+                messageHTML += '</div>' +
+                    '<p class="msg-date msg-date-sent">' + formatTime(new Date(messages[x].dateTime)) + '</p>' +
+                    '</div>' +
+                    '</div>';
+            }
+        } else {
+            if (messages[x].type == "file") {
+                messageHTML += '<div class="sent-wrapper">' +
+                    '<div class="containMessageSent">' +
+                    '<div class="msg_">' +
+                    '<p class="msgSender"></p>' +
+                    '<p class="messageBody sentMsg bg-primary message-attachment">';
+                if (messages[x].content.includes(".pdf")) {
+                    messageHTML += '<iframe class="pdf-iframe" src="' + messages[x].content + '"></iframe>' +
+                        '<p class="text-center"><a target="_blank" class="text-light"' +
+                        'href="' + messages[x].content + '">View Full Screen</a></p>';
+                } else {
+                    messageHTML += '<a target="_blank" href="<%=messages[x].content%>">' +
+                        '<img src="' + messages[x].content + '" />' +
+                        '</a>';
+                }
+                messageHTML += '</p>' +
+                    '</div>' +
+                    '<p class="msg-date msg-date-sent">' + formatTime(new Date(messages[x].dateTime)) +
+                    '</p>' +
+                    '</div>' +
+                    '</div>';
+
+            } else {
+                messageHTML += '<div class="sent-wrapper">' +
+                    '<div class="containMessageSent">' +
+                    '<div class="msg_">';
+                if (messages[x].youtubeData[0] != undefined) {
+                    messageHTML += '<a target="_blank" href="' + messages[x].youtubeData[0].link + '">' +
+                        '<div class="messageBody sentMsg bg-primary">' +
+                        '<img class="yt-logo" src= "../assets/img/yt-logo.svg"/>' +
+                        '<div class="thumbnail-container">' +
+                        '<img src="' + messages[x].youtubeData[0].thumbnail + '" class="yt-thumbnail"/>' +
+                        '</div>' +
+                        '<span class="text-light yt-title">' + unescapeApostrophe(messages[x].youtubeData[0].title) + '</span>' +
+                        '</div>' +
+                        '</a>';
+                } else {
+                    messageHTML += '<p class="messageBody sentMsg bg-primary">' + messages[x].content + '</p>';
+                }
+                messageHTML += '</div>' +
+                    '<p class="msg-date msg-date-sent">' + formatTime(new Date(messages[x].dateTime)) + '</p>' +
+                    '</div>' +
+                    '</div>';
+            }
+        }
+    }
+    return messageHTML;
 }
--- assets/js/Messages/messageScroll.js
+++ assets/js/Messages/messageScroll.js
@@ -4,103 +4,7 @@ function isInViewport(el) {
         return true;
     }
 }
-function prependNext50(messages, handle, block) {
-    var messageHTML = '<div class="messageTop'+block+'"></div>';
-    for (var x = messages.length - 1; x >= 0; x--) {
-        // check if session handle === sender
-        if (handle === messages[x].sender) {
-            if (messages[x].type == "file") {
-                messageHTML += '<div class="sent-wrapper">' +
-                    '<div class="containMessageSent">' +
-                    '<div class="msg_">' +
-                    '<p class="msgSender"></p>' +
-                    '<p class="messageBody sentMsg bg-primary message-attachment">';
-                if (messages[x].content.includes(".pdf")) {
-                    messageHTML += '<iframe class="pdf-iframe" src="' + messages[x].content + '"></iframe>' +
-                        '<p class="text-center"><a target="_blank" class="text-light"' +
-                        'href="' + messages[x].content + '">View Full Screen</a></p>';
-                } else {
-                    messageHTML += '<a target="_blank" href="<%=messages[x].content%>">' +
-                        '<img src="' + messages[x].content + '" />' +
-                        '</a>';
-                }
-                messageHTML += '</p>' +
-                    '</div>' +
-                    '<p class="msg-date msg-date-sent">' + formatTime(new Date(messages[x].dateTime)) +
-                    '</p>' +
-                    '</div>' +
-                    '</div>';
-            } else {
-                messageHTML += '<div class="sent-wrapper">' +
-                    '<div class="containMessageSent">' +
-                    '<div class="msg_">';
-                if (messages[x].youtubeData[0] != undefined) {
-                    messageHTML += '<a target="_blank" href="' + messages[x].youtubeData[0].link + '">' +
-                        '<div class="messageBody sentMsg bg-primary">' +
-                        '<img class="yt-logo" src= "../assets/img/yt-logo.svg"/>' +
-                        '<div class="thumbnail-container">' +
-                        '<img src="' + messages[x].youtubeData[0].thumbnail + '" class="yt-thumbnail"/>' +
-                        '</div>' +
-                        '<span class="text-light yt-title">' + unescapeApostrophe(messages[x].youtubeData[0].title) + '</span>' +
-                        '</div>' +
-                        '</a>';
-                } else {
-                    messageHTML += '<p class="messageBody sentMsg bg-primary">' + messages[x].content + '</p>';
-                }
-                messageHTML += '</div>' +
-                    '<p class="msg-date msg-date-sent">' + formatTime(new Date(messages[x].dateTime)) + '</p>' +
-                    '</div>' +
-                    '</div>';
-            }
-        } else {
-            if (messages[x].type == "file") {
-                messageHTML += '<div class="sent-wrapper">' +
-                    '<div class="containMessageSent">' +
-                    '<div class="msg_">' +
-                    '<p class="msgSender"></p>' +
-                    '<p class="messageBody sentMsg bg-primary message-attachment">';
-                if (messages[x].content.includes(".pdf")) {
-                    messageHTML += '<iframe class="pdf-iframe" src="' + messages[x].content + '"></iframe>' +
-                        '<p class="text-center"><a target="_blank" class="text-light"' +
-                        'href="' + messages[x].content + '">View Full Screen</a></p>';
-                } else {
-                    messageHTML += '<a target="_blank" href="<%=messages[x].content%>">' +
-                        '<img src="' + messages[x].content + '" />' +
-                        '</a>';
-                }
-                messageHTML += '</p>' +
-                    '</div>' +
-                    '<p class="msg-date msg-date-sent">' + formatTime(new Date(messages[x].dateTime)) +
-                    '</p>' +
-                    '</div>' +
-                    '</div>';
 
-            } else {
-                messageHTML += '<div class="sent-wrapper">' +
-                    '<div class="containMessageSent">' +
-                    '<div class="msg_">';
-                if (messages[x].youtubeData[0] != undefined) {
-                    messageHTML += '<a target="_blank" href="' + messages[x].youtubeData[0].link + '">' +
-                        '<div class="messageBody sentMsg bg-primary">' +
-                        '<img class="yt-logo" src= "../assets/img/yt-logo.svg"/>' +
-                        '<div class="thumbnail-container">' +
-                        '<img src="' + messages[x].youtubeData[0].thumbnail + '" class="yt-thumbnail"/>' +
-                        '</div>' +
-                        '<span class="text-light yt-title">' + unescapeApostrophe(messages[x].youtubeData[0].title) + '</span>' +
-                        '</div>' +
-                        '</a>';
-                } else {
-                    messageHTML += '<p class="messageBody sentMsg bg-primary">' + messages[x].content + '</p>';
-                }
-                messageHTML += '</div>' +
-                    '<p class="msg-date msg-date-sent">' + formatTime(new Date(messages[x].dateTime)) + '</p>' +
-                    '</div>' +
-                    '</div>';
-            }
-        }
-    }
-    return messageHTML;
-}
 
 
 $(document).ready(function () {
@@ -113,8 +17,6 @@ $(document).ready(function () {
 
     $(".messageBlock").on("scroll", () => {
         if(document.querySelector(".messageTop"+block)) {
-
-       
         if (isInViewport(document.querySelector(".messageTop"+block))) {
             //make ajax request to get the next set of 50 messages
             var oldMessageTop = $(".messageTop"+block);
