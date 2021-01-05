524bcd12998529e439476b7f72fd039e3c8d4b12
Connor Boothe <connorboothe@Connors-MBP.lan1>
2021-01-05 12:58:44 -0500

    Created frontend post route to update major on the User Profile page

---
+++ assets/js/UserAccount/updateMajor.js
@@ -0,0 +1,40 @@
+
+$(document).ready(()=> {
+    //clear border on focus
+   $("#searchInput").on("focus", function(){
+       $(this).parent().css({border: "none"})
+   })
+   //submit change major POST request on button click
+    $(".editMajor").on("click", function(){
+        if($("#major").text() == "") {
+            $(".input-container1Light-settings").css({border: "1px solid #dc3545"});
+        }
+        else{
+            payload = {
+                major:$("#major").text(),
+              }
+            $.ajax({
+                url: "/updateMajor",
+                type: 'POST',
+                data: JSON.stringify(payload),
+                headers: {
+                  "Content-Type": "application/json"
+                }, statusCode: {
+                  202: function (result) {
+                    $(".majorSpan").text(result.major);
+                    $(".modal").modal("hide");
+                    $(".newMajor").html("")
+                    
+                  },
+                  500: function (result) {
+                    alert("500 " + result.responseJSON.err);
+                  },
+                },
+              });
+          
+        }
+        })
+    
+    
+   
+})
