dc4a5ba9109346591626fe7c140500a6bdfda8a0
Connor Boothe <connorboothe@Connors-MBP.lan1>
2021-01-15 03:07:10 -0500

    Added story crop

--- assets/js/crop.js
+++ assets/js/crop.js
@@ -101,8 +101,8 @@ $(document).ready(function () {
         type = "story";
         $uploadCrop = $('.content-container-add-story').croppie({
           viewport: {
-            width: 400,
-            height: 500,
+            width: 500,
+            height: 700,
             type: 'rectangle',
           },
           boundary: {
@@ -340,59 +340,133 @@ $(document).ready(function () {
     $(".overlay").hide();
     $(".img-upload-container").hide();
   })
+
   $(".add-story-btn").on("click", function () {
-    $uploadCrop.croppie('result', {
-      type: 'canvas',
-      size: { width: 800 },
-      format: 'jpg',
-    }).then(function (resp) {
-      var filename = $("#addImage").val().replace(/C:\\fakepath\\/i, '')
-      var storageRef = firebase.storage().ref("/groupImages/" + $(".groupName").text() + "/storyImages/" + filename);
-      var image = base64ImageToBlob(resp);
-      // console.log(image)
-      var metadata = {
-        contentType: 'image/jpeg',
-      };
-      var task = storageRef.put(image, metadata);
-      task.on("state_changed", function () {
-        function error(error) {
-          alert(error);
-        }
-      })
-
-      storageRef.getDownloadURL().then(function (url) {
-        payload = {
-          url: url,
-          groupId: $("input[name='groupId']").val(),
-          text: $(".story-text-span").text(),
-          duration: $(".story-duration").val()
-        }
-        alert(payload.duration)
-        $.ajax({
-          url: "/addStory",
-          type: 'POST',
-          data: JSON.stringify(payload),
-          headers: {
-            "Content-Type": "application/json"
-          }, statusCode: {
-            202: function (result) {
-              $(function () {
-                $(".modal").modal("hide");
-                $(".bannerSuccess").text("Story successfully posted!")
-                $(".bannerSuccess").fadeIn();
-                setTimeout(() => {
-                  $(".bannerSuccess").fadeOut();
-                }, 2500)
-              })
-
+    var type = $("input[name='story-type']").val()
+    if(type == "image"){
+      $uploadCrop.croppie('result', {
+        type: 'canvas',
+        size: { width: 800 },
+        format: 'jpg',
+      }).then(function (resp) {
+        var filename = $("#addImage").val().replace(/C:\\fakepath\\/i, '')
+        var storageRef = firebase.storage().ref("/groupImages/" + $(".groupName").text() + "/storyImages/" + filename);
+        var image = base64ImageToBlob(resp);
+        // console.log(image)
+        var metadata = {
+          contentType: 'image/jpeg',
+        };
+        var task = storageRef.put(image, metadata);
+        task.on("state_changed", function () {
+          function error(error) {
+            alert(error);
+          }
+        })
+  
+        storageRef.getDownloadURL().then(function (url) {
+          payload = {
+            type:"image",
+            url: url,
+            groupId: $("input[name='groupId']").val(),
+            text: $(".story-text-span").text(),
+            duration: $(".story-duration").val()
+          }
+          $.ajax({
+            url: "/addStory",
+            type: 'POST',
+            data: JSON.stringify(payload),
+            headers: {
+              "Content-Type": "application/json"
+            }, statusCode: {
+              202: function (result) {
+                $(function () {
+                  $(".modal").modal("hide");
+                  $(".bannerSuccess").text("Story successfully posted!")
+                  $(".bannerSuccess").fadeIn();
+                  setTimeout(() => {
+                    $(".bannerSuccess").fadeOut();
+                  }, 2500)
+                })
+  
+              },
+              500: function (result) {
+                alert("500 " + result.responseJSON.err);
+              },
             },
-            500: function (result) {
-              alert("500 " + result.responseJSON.err);
-            },
-          },
+          });
         });
+      })
+    }
+    else if(type == "poll") {
+      payload = {
+        type:"poll",
+        question: $("#poll-question").text(),
+        option1: $(".poll-answer").eq(0).text(),
+        option2:$(".poll-answer").eq(1).text(),
+        groupId: $("input[name='groupId']").val(),
+        duration: $(".story-duration").val()
+      }
+      $.ajax({
+        url: "/addStory",
+        type: 'POST',
+        data: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json"
+        }, statusCode: {
+          202: function (result) {
+            $(function () {
+              $(".modal").modal("hide");
+              $(".bannerSuccess").text("Story successfully posted!")
+              $(".bannerSuccess").fadeIn();
+              setTimeout(() => {
+                $(".bannerSuccess").fadeOut();
+              }, 2500)
+            })
+
+          },
+          500: function (result) {
+            alert("500 " + result.responseJSON.err);
+          },
+        },
       });
-    })
+      console.log(payload)
+    }
+    else if(type == "multiple") {
+      payload = {
+        type:"multiple",
+        question: $("#multiple-question").text(),
+        option1: $(".answer-text").eq(0).text(),
+        option2:$(".answer-text").eq(1).text(),
+        option3: $(".answer-text").eq(2).text(),
+        option4:$(".answer-text").eq(3).text(),
+        groupId: $("input[name='groupId']").val(),
+        duration: $(".story-duration").val(),
+        correct: $("input[name='correct']:checked").parent().prev().text()
+      }
+      $.ajax({
+        url: "/addStory",
+        type: 'POST',
+        data: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json"
+        }, statusCode: {
+          202: function (result) {
+            $(function () {
+              $(".modal").modal("hide");
+              $(".bannerSuccess").text("Story successfully posted!")
+              $(".bannerSuccess").fadeIn();
+              setTimeout(() => {
+                $(".bannerSuccess").fadeOut();
+              }, 2500)
+            })
+
+          },
+          500: function (result) {
+            alert("500 " + result.responseJSON.err);
+          },
+        },
+      });
+    }
   })
 })
 
