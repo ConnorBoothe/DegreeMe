e9b945b3877ca729a1a3aaed191d8baa92a9a651
Connor Boothe <connorboothe@Connors-MacBook-Pro.local>
2021-03-15 23:24:27 -0400

    Removed comments validation (old)

--- assets/js/commentsValidation.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function(){
-//     $("#comments-button").on("click",  function(e){
-//         if($(".commentMsg").val() === ""){
-//             e.preventDefault();
-//             $(".commentMsg").css("border", "1px solid #dc3545");
-//         }
-//     })
-   
-// })
